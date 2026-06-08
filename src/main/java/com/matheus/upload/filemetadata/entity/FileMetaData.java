1c83ca18830ba111a6897396d5fec4fb017ea48f
Matheus Silva <mathsilva.code@gmail.com>
2026-06-08 15:43:11 -0300

    feat: add FileMetaData entity

---
+++ src/main/java/com/matheus/upload/filemetadata/entity/FileMetaData.java
@@ -0,0 +1,29 @@
+package com.matheus.upload.filemetadata.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "file_meta_data")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FileMetaData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String originalName;
+
+    private String storedName;
+
+    private Long size;
+
+    @CreationTimestamp
+    private Instant uploadDate;
+}
